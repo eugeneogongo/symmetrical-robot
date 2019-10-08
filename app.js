a7b5dae3534306c3c4816adce03fcb82ee675c99
Neptune <eugeneogongo@live.com>
2019-10-08 16:11:20 +0300

    CORS Middleware

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
 var bodyParser =  require("body-parser");
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({credentials: true,origin: 'https://mytweetstream.herokuapp.com/'}));
 io.origins('*:*');
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
 
 var Twitter = require('node-tweet-stream')
     , tw = new Twitter({
